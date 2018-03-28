63d2fa3f6886f81cc6c709bc428f2d00536560c3
Olle Wahlström <olle.wahlstrom@a3j.se>
2018-03-28 18:58:20 +0200

    alot of stuff

--- routes/index.js
+++ routes/index.js
@@ -9,9 +9,17 @@ router.get('/', (req, res, next) => {
 
 router.get('/posts', (req, res, next) => {
   console.log("getting posts")
+  const posts = []
+  const post = {
+    date: new Date(),
+    title: "Test wow",
+    body: "Idag gjorde sara nått dumt. Hon snubblade på ett bananskal så att hela USA skrattade. \
+            Sen snubbladne hon igen och fick banskalet i munnen så hon nästan kräktes.. haha hoho",
+  }
+  posts.push(post)
   return res.json({
     status: "ok",
-    posts: []
+    posts
   })
 })
 
